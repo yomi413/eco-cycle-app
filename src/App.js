cf695522c6db97248314953b032280317441fcbd
Yomaira Escano <yjescano@gmail.com>
2019-10-26 16:13:11 -0400

    Add <Map /> component and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./Map";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to EcoCycle!</h1>
+
+      <h4>
+        Our goal is to provide a system that can simplify how to determine what
+        gets recycled and how, where to find the nearest water fountains and
+        where to find the nearest trash cans.{" "}
+      </h4>
+
+      <Map />
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Map.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+  render() {
+    return <h1>Map goes here</h1>;
+  }
+}
+
+export default Map;
