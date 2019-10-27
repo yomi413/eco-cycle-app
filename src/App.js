6005aaf54a9404c6da2a203fb6f7cea257892453
Yomaira Escano <yjescano@gmail.com>
2019-10-26 23:04:42 -0400

    Imported new component features

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Map from "./Map";
+import Search from "./Search";
+import NavBar from "./NavBar";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to EcoCycle!</h1>
+      <NavBar />
+      <center>
+        <h1>Welcome to EcoCycle!</h1>
 
-      <h4>
-        Our goal is to provide a system that can simplify how to determine what
-        gets recycled and how, where to find the nearest water fountains and
-        where to find the nearest trash cans.{" "}
-      </h4>
+        <h4>
+          Our goal is to provide a system that can simplify how to determine
+          what gets recycled and how, where to find the nearest water fountains
+          and where to find the nearest trash cans.{" "}
+        </h4>
 
-      <Map />
+        <Search />
+
+        <Map />
+      </center>
     </div>
 
     // <div className="App">
