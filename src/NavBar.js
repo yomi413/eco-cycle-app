e71b8456697c70ea0bf9feb4d9a0b026b0209d9a
Yomaira Escano <yjescano@gmail.com>
2019-10-27 09:20:03 -0400

    Changed navigation bar color

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 class NavBar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <Navbar collapseOnSelect expand="lg" bg="primary" variant="light">
+        <Navbar collapseOnSelect expand="lg" className="navbar">
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="/water-fountains">Water Fountains</Nav.Link>
               <Nav.Link href="/trash-cans">Trash Cans</Nav.Link>
             </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
           </Navbar.Collapse>
         </Navbar>
       </div>
