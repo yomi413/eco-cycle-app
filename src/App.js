65decf9815e928be8748e21da74ff7c449f3d500
Yomaira Escano <yjescano@gmail.com>
2019-10-27 09:18:44 -0400

    Modified Objective; added Who We Are and Join Us! paragraphs

--- src/App.js
+++ src/App.js
@@ -11,36 +11,47 @@ function App() {
     <div>
       <NavBar />
       <center>
-        <h1>Welcome to EcoCycle!</h1>
-
-        <h4>
-          Our goal is to provide a system that can simplify how to determine
-          what gets recycled and how, where to find the nearest water fountains
-          and where to find the nearest trash cans.{" "}
-        </h4>
-
-        <Search />
-
-        <Map />
+        <i>
+          <h4>To reuse, to recycle, to be resourceful New Yorkers.</h4>
+        </i>
       </center>
-    </div>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+      <center>
+        <h1>Welcome to EcoCycle</h1>
+      </center>
+
+      <h3>Who We Are</h3>
+      <p>
+        EcoCycle is a company dedicated to help the environment by providing a
+        way for New Yorkers to dispose of their trash and recyclables properly,
+        as well as helping find water fountains that will allow for refilling
+        reusable water bottles.{" "}
+      </p>
+
+      <h3>Objective</h3>
+      <p>
+        Don't you hate walking on the street and having to hold on to garbage
+        because you can't find a trash bin? How about being unsure whether the
+        item you want to dispose of is recyclable or not? Or what if you want to
+        reuse your water bottle instead of disposing it? These are some of the
+        problems that EcoCycle wants to tackle. We aim to assist users in New
+        York City in finding proper trash and recycling bins in public spaces,
+        as well as finding good sources of drinking water from clean and usable
+        water fountains. Better yet, we want to do this with the help of all New
+        Yorkers! By providing a rating system for the water fountains, users
+        will be able to get information on how clean the water fountain near
+        them is. All users can benefit from having access to basic information
+        that can help keep our city streets clean. We help the environment while
+        also being resourceful.
+      </p>
+
+      <h3>Join Us!</h3>
+      <p>
+        <a href="#">Download</a> the app and join us in saving our environment!
+      </p>
+
+      <Map />
+    </div>
   );
 }
 
