15cc116ea5f5242be7b7cadf9567ef43a565d642
Yomaira Escano <yjescano@gmail.com>
2019-10-27 09:19:23 -0400

    Modified map information

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 
 class Map extends Component {
   //   state = {
@@ -6,49 +7,122 @@ class Map extends Component {
 
   //   };
 
-  componentDidMount() {
-    fetch(
-      "https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=water+fountains+in+new+york+city",
-      {
-        credentials: "include",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
-      }
-    );
-    //   .then(response => console.log(response.json()))
-    //   .then(data => {
-    //     this.setState({
-    //       location: data
-    //     });
-    //   });
-  }
+  // componentDidMount() {
+  //   fetch(
+  //     "https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=water+fountains+in+new+york+city",
+  //     {
+  //       credentials: "include",
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json"
+  //       }
+  //     }
+  //   );
+  //   .then(response => console.log(response.json()))
+  //   .then(data => {
+  //     this.setState({
+  //       location: data
+  //     });
+  //   });
+  // }
 
   render() {
     return (
-      <div>
-        <h1>Map of Water Fountains in New York City</h1>
-        <iframe
-          width="450"
-          height="250"
-          frameborder="0"
-          //   style="border:0"
-          src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=water+fountains+in+new+york+city"
-          allowFullScreen
-        ></iframe>
+      <Table className="table">
+        <thead>
+          <tr>
+            <th>
+              <center>
+                <h6 id="map-header-one">Map of Water Fountains</h6>
+              </center>
+            </th>
+            <th>
+              <center>
+                <h6 id="map-header-two">Map of Trash Bins</h6>
+              </center>
+            </th>
+            <th>
+              <center>
+                <h6>Map of Recycling Bins</h6>
+              </center>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <iframe
+                width="250"
+                height="150"
+                frameborder="0"
+                //   style="border:0"
+                src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=water+fountains+in+new+york+city"
+                allowFullScreen
+              ></iframe>
+            </td>
 
-        <h1>Map of Trash Cans in New York City</h1>
-        <iframe
-          width="450"
-          height="250"
-          frameborder="0"
-          //   style="border:0"
-          src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=trash+cans+in+new+york+city"
-          allowFullScreen
-        ></iframe>
-      </div>
+            <td>
+              <iframe
+                width="250"
+                height="150"
+                frameborder="0"
+                //   style="border:0"
+                src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=trash+cans+in+new+york+city"
+                allowFullScreen
+              ></iframe>
+            </td>
+
+            <td>
+              <iframe
+                width="250"
+                height="150"
+                frameborder="0"
+                //   style="border:0"
+                src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=recycling+bins+in+new+york+city"
+                allowFullScreen
+              ></iframe>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      // <div>
+      //   <center>
+      //     <h3 id="map-header-one">Map of Water Fountains in New York City</h3>
+
+      //     <iframe
+      //       width="450"
+      //       height="250"
+      //       frameborder="0"
+      //       //   style="border:0"
+      //       src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=water+fountains+in+new+york+city"
+      //       allowFullScreen
+      //     ></iframe>
+
+      //     <h3 id="map-header-two">Map of Trash Bins in New York City</h3>
+
+      //     <iframe
+      //       width="450"
+      //       height="250"
+      //       frameborder="0"
+      //       //   style="border:0"
+      //       src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=trash+cans+in+new+york+city"
+      //       allowFullScreen
+      //     ></iframe>
+
+      //     <h3>Map of Recycling Bins in New York City</h3>
+
+      //     <iframe
+      //       width="450"
+      //       height="250"
+      //       frameborder="0"
+      //       //   style="border:0"
+      //       src="https://www.google.com/maps/embed/v1/search?key=AIzaSyByJVMVmqeE0wxBjw2XtKpLSUNh9sk06Q8&q=recycling+bins+in+new+york+city"
+      //       allowFullScreen
+      //     ></iframe>
+      //   </center>
+      // </div>
     );
   }
 }
